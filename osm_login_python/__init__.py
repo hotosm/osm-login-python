7c9d263a5577408f7562fe7cdc007ae9180ecfee
spwoodcock <sam.woodcock@protonmail.com>
2024-08-07 17:45:39 +0100

    ci: fix linting errors running pre-commit hooks

--- osm_login_python/__init__.py
+++ osm_login_python/__init__.py
@@ -1,9 +1,15 @@
+"""Models to return validated JSON."""
+
 from pydantic import BaseModel
 
 
 class Login(BaseModel):
+    """Model to return the login url."""
+
     login_url: str
 
 
 class Token(BaseModel):
+    """Model to return the access token."""
+
     access_token: str
